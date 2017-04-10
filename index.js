52ee6cb3d4d367890d5c8b46b1ae894c066a552b
Basim Hennawi <basim.a.hennawi@gmail.com>
2017-04-10 16:51:34 +0200

    Recieve message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
   console.log('A user connected!');
   socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
+    io.emit('chat message', msg)
   });
   socket.on('disconnect', function() {
     console.log('A user disconnected!');
