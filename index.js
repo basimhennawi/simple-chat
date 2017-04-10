8ceb43023525d8a03bbaf4ce0962e9fd98963d10
Basim Hennawi <basim.a.hennawi@gmail.com>
2017-04-10 16:40:59 +0200

    Integrate socket.io

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected!');
+  socket.on('disconnect', function(){
+    console.log('A user disconnected!');
+  });
 });
 
 http.listen(8080, function() {
